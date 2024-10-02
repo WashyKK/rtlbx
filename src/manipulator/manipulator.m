d56eff3346b3b0b0ecb0c3083a256f4f3d4a4857
washingtonkk <washingtonkigan@gmail.com>
2024-10-02 04:49:48 +0300

    finalize 3DOF project
    Signed-off-by: washingtonkk <washingtonkigan@gmail.com>

--- src/manipulator/manipulator.m
+++ src/manipulator/manipulator.m
@@ -1,3 +1,4 @@
+clear; clc;
 % PROJECT
 
 % 3 DOF project
@@ -5,52 +6,49 @@
 % From the computed D-H parameters, the robot is defined with three links
 
 L(1) = Link([0, 10, 0, deg2rad(-90), 0]);
-L(2) = Link([0, 5, 10, deg2rad(0), 0]);
-L(3) = Link([0, 5, 10, deg2rad(0), 0]);
+L(2) = Link([0, 0.5, 10, deg2rad(0), 0]);
+L(3) = Link([0, 0.5, 10, deg2rad(0), 0]);
 
-robot = SerialLink(L, 'name', 'washingtons robot');
-robot; % Display the robot details
+wkk_robot = SerialLink(L, 'name', 'washingtons robot');
+wkk_robot; % Display the robot details
 
 % Plot the robot at all angles = 0 radians
 % This is my default robot's position
 % robot.plot([0 0 0]); 
 
-T = robot.fkine([pi/3, pi/4, pi/5]);
-% robot.plot([pi/3, pi/4, pi/5]);
+home_position = [0, -1*pi/2, 0];
 
 % The mask option must be specified for under manipulated robots: i.e., robots with less than 6 DOF
-IT = robot.ikine(T, 'mask', [1 1 1 0 0 0]);
-% robot.plot(IT);
+% T = robot.fkine([pi/3, pi/4, pi/5]);
+% IT = robot.ikine(T, 'mask', [1 1 1 0 0 0]);
+% wkk_robot.plot(IT);
 
 % Robot motion between two orientations in the space
-% We need to find out why the below does not accept inverse kinematics
+% I need to find out why the below does not accept inverse kinematics
 % Use a for loop to iterate over the forward kinematic to get the workspace limits
 % T1 = transl(0.4, 0.2, 0) * trotx(pi);
-% T2 = transl(0.4, -0.2, 0) * trotx(pi/2);
+% T21 = transl(-20, -2, 10) * trotx(pi/2); -> Does not  work because the robot is very under manipulated
 
-T1 = robot.fkine([deg2rad(5), deg2rad(30), deg2rad(15)]);
-T2 = robot.fkine([deg2rad(70), deg2rad(130), deg2rad(160)]);
+T1 = wkk_robot.fkine(home_position);
+T2 = wkk_robot.fkine([deg2rad(20), deg2rad(-10), deg2rad(18)]);
+T3 = wkk_robot.fkine([deg2rad(-180), deg2rad(-180), deg2rad(180)]);
+T4 = wkk_robot.fkine([deg2rad(240), deg2rad(120), deg2rad(290)]);
 
 % The inverse kinematic of these is given by:
-IT1 = robot.ikine(T1, 'mask', [1 1 1 0 0 0]);
-IT2 = robot.ikine(T2, 'mask', [1 1 1 0 0 0]);
-T1T = robot.fkine([IT1(1), IT1(2), IT1(3)]);
+IT1 = wkk_robot.ikine(T1, 'mask', [1 1 1 0 0 0]);
+IT2 = wkk_robot.ikine(T2, 'mask', [1 1 1 0 0 0]);
+IT3 = wkk_robot.ikine(T3, 'mask', [1 1 1 0 0 0]);
+IT4 = wkk_robot.ikine(T4, 'mask', [1 1 1 0 0 0]);
 
 % Time for robot motion
 t = [0:0.05:2]';
 
 % Generate the trajectory using linear segments with parabolic blends
-q = mtraj(@lspb, IT1, IT2, t);
-q1 = jtraj(IT1, IT2, t); % Convenience function that is equivalent to mtraj with tpoly
+q = mtraj(@lspb, IT1, IT3, t);
+q1 = jtraj(IT3, IT4, t); % Convenience function that is equivalent to mtraj with tpoly -> A higher order polynomial (Order 5)
 
-%To obtain and plot all vectors
-[q1, q1d, q1dd] = jtraj(IT1, IT2, t);
-
-[q, qd, qdd] = mtraj(@lspb, IT1, IT2, t);
-
-robot.jtraj(T1, T2, t, 'mask', [1 1 1 0 0 0])
-robot.tool = trotx(deg2rad(130));
-robot.plot(q)
+wkk_robot.plot(q, 'loop', 'trail', 'r');
+title('Robot Motion');
 
 % What we did above:
 % Get the inverse kinematic angles theta 1 2 and 3
