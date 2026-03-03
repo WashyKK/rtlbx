a384c62805a1bec3889c7c3658222ca78d0ccb0e
WashingtonKK <washingtonkigan@gmail.com>
2026-03-03 16:01:20 +0300

    one more
    Signed-off-by: WashingtonKK <washingtonkigan@gmail.com>

--- main.go
+++ main.go
@@ -14,7 +14,8 @@ func main() {
 
 	fmt.Println("Current time:", time.Now().Format(time.RFC3339))
 
-	fmt.Println("Time:", time.Now().Format(time.RFC3339))
 	
+	fmt.Println("Time:", time.Now().Format(time.RFC3339))
+
 
 }
